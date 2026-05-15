8bab249c23db64c111f9cbf632b55c18249e5210
Anderson Nieves <andersonvnieves@live.com>
2026-05-14 22:54:45 -0300

    Add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
+import { useAuth } from "react-oidc-context";
 import { MkButton } from 'moldekit-react';
 
 function App() {
+    const auth = useAuth();
 
-  return (
-    <section className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="font-bold text-center text-4xl">mangos</h1>
-      <h2 className="text-center text-2xl">under construction !</h2>
+    const signOutRedirect = () => {
+        const clientId = "5tu7j65i3d66r1qtrlu8k7os91";
+        const logoutUri = "http://localhost:5173/";
+        const cognitoDomain = "https://sa-east-1oechf8sjw.auth.sa-east-1.amazoncognito.com";
+        window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+    };
 
-        <MkButton iconName="home" variant="filled" color="primary" onClick={() => alert('clicked')}>
-          Home
-        </MkButton>
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
 
-    </section>
-  )
+    if (auth.error) {
+        return <div>Encountering error... {auth.error.message}</div>;
+    }
+
+    if (auth.isAuthenticated) {
+        return (
+            <section className="flex flex-col items-center justify-center min-h-screen">
+                <h1 className="font-bold text-center text-4xl">mangos</h1>
+                <pre> Hello: {auth.user?.profile.email} </pre>
+                <pre> ID Token: {auth.user?.id_token} </pre>
+                <pre> Access Token: {auth.user?.access_token} </pre>
+                <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+                <MkButton iconName="home" variant="filled" color="primary" onClick={() => auth.removeUser()}>
+                    Sign out
+                </MkButton>
+
+            </section>
+        );
+    }
+
+    return (
+        <div>
+            <button onClick={() => auth.signinRedirect()}>Sign in</button>
+            <button onClick={() => signOutRedirect()}>Sign out</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { AuthProvider } from "react-oidc-context";
 import App from './App.tsx'
+
+const cognitoAuthConfig = {
+    authority: "https://cognito-idp.sa-east-1.amazonaws.com/sa-east-1_oEchf8sJw",
+    client_id: "5tu7j65i3d66r1qtrlu8k7os91",
+    redirect_uri: "http://localhost:5173/",
+    response_type: "code",
+    scope: "email openid",
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <AuthProvider {...cognitoAuthConfig}>
+          <App />
+      </AuthProvider>
   </StrictMode>,
 )
