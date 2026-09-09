323f12095c7f718c47a1f7efcae6d4b088540585
Anderson Nieves <andersonvnieves@live.com>
2026-09-09 00:16:56 -0300

    Continuação da barra lateral direita

--- src/app/components/RightSidebar.tsx
+++ src/app/components/RightSidebar.tsx
@@ -1,10 +1,8 @@
-import {MkSubHeader} from "moldekit-react";
 
 function RightSidebar( { children }: { children?: React.ReactNode }) {
 
 
     return (<section className={`flex flex-col justify-between h-svh w-[320px]`}>
-        <MkSubHeader title={"Teste"} subtitle={"Teste"}/>
         { children }
     </section>);
 }
--- src/app/layout/AppLayout.tsx
+++ src/app/layout/AppLayout.tsx
@@ -16,11 +16,13 @@ import {useAuth} from "react-oidc-context";
 import MangosLogo from "../components/MangosLogo.tsx";
 import {Outlet} from "react-router-dom";
 import RightSidebar from "../components/RightSidebar.tsx";
+import {RightSidebarProvider, useRightSidebar} from "../../shared/context/RightSidebarContext.tsx";
 
-function AppLayout() {
+function AppLayoutInner() {
     const isMobile = useMediaQuery("(max-width: 768px)");
     const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
     const auth = useAuth();
+    const { content } = useRightSidebar();
 
     return (
         <div className={isMobile ? "flex flex-col" : "flex flex-row"}>
@@ -55,10 +57,18 @@ function AppLayout() {
         ><MangosLogo/>
         </MkTopBar> }
 
-        { !isMobile && <LeftSidebar className={"pt-4 pb-4 pl-4 pr-2"}/>}
+        { !isMobile && <LeftSidebar className={"pt-4 pb-4 pl-4 pr-2"}/> }
         <section className={`flex-1 pt-4 pb-4 ${ isMobile ? "pl-4" : "pl-2" } pr-4`}><MkMain><Outlet /></MkMain></section>
-            { !isMobile && <RightSidebar /> }
+            { !isMobile && <RightSidebar>{ content }</RightSidebar> }
     </div>)
+}
+
+function AppLayout() {
+    return (
+        <RightSidebarProvider>
+            <AppLayoutInner />
+        </RightSidebarProvider>
+    );
 }
 
 export default AppLayout;
